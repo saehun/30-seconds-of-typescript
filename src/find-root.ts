357114d6378256880bceccd4d40f2806cf4a1a0b
minidonut <nycom13@gmail.com>
2020-08-09 18:21:19 +0900

    add: find-root

---
+++ src/find-root.ts
@@ -0,0 +1,16 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export const findRecursive = (target: string) => (start = process.cwd()): string => {
+  let dir = start;
+
+  while (!fs.existsSync(path.join(dir, target))) {
+    if (dir === '/') throw new Error(`Can't find ${target}'`);
+    dir = path.join(dir, '..');
+  }
+
+  return dir;
+};
+
+export const findGitRoot = findRecursive('.git');
+export const findNpmRoot = findRecursive('package.json');
