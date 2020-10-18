a3dcbd7dfa6515071b4aaeee492e8e71de22927e
minidonut <nycom13@gmail.com>
2020-10-18 21:42:35 +0900

    add uncaughtError

--- src/error.ts
+++ src/error.ts
@@ -8,3 +8,16 @@ export class FildNotFoundError extends Error {
     super(message);
   }
 }
+
+/**
+ * handle final exception, and promise rejection which didn't caught
+ */
+export const handleUncaughtException = (handle: (error: Error) => never): void => {
+  process.on('uncaughtException', error => {
+    handle(error);
+  });
+
+  process.on('unhandledRejection', async function (error) {
+    throw error;
+  });
+};
