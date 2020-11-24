e6971afa3609265ef0d55c858d08c6cf91941118
minidonut <nycom13@gmail.com>
2020-11-24 13:09:30 +0900

    add promisify wrapper

--- src/wrapper-function.ts
+++ src/wrapper-function.ts
@@ -6,3 +6,12 @@ export function wrap<A extends any[], R>(fn: (...a: A) => R) {
     return fn(...args);
   };
 }
+
+/**
+ * Promisify wrapper
+ */
+export function wrapPromise<A extends any[], R>(fn: (...a: A) => R) {
+  return async function (...args: A): Promise<R> {
+    return fn(...args);
+  };
+}
