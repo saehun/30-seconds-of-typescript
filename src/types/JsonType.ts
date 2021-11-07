83ee79db73d3eb6128dbb12c2fdecf1a47b69136
minidonut <nycom13@gmail.com>
2021-11-08 02:13:14 +0900

    feat: add DTO type

--- src/types/JsonType.ts
+++ src/types/JsonType.ts
@@ -1,3 +1,5 @@
+import { expectAssignable, expectNotAssignable } from 'tsd';
+
 type Json = string | number | boolean | null | Json[] | { [key: string]: Json };
 
 const exampleStatusJSON: Json = {
@@ -9,3 +11,22 @@ const exampleStatusJSON: Json = {
     // update: () => {},
   },
 };
+
+type DataPropertyNames<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? never : K;
+}[keyof T];
+
+type DataPropertiesOnly<T> = {
+  [P in DataPropertyNames<T>]: T[P] extends Record<string, unknown> ? DTO<T[P]> : T[P];
+};
+
+export type DTO<T> = DataPropertiesOnly<T>;
+
+type Ticket = {
+  name: string;
+  available(now: number): boolean;
+};
+
+expectAssignable<Ticket>({ name: '', available: () => true });
+expectAssignable<DTO<Ticket>>({ name: '' });
+expectNotAssignable<DTO<Ticket>>({ name: '', available: () => true });
