2fd2b596ecd10102d39001582cb0373f51539b1b
minidonut <nycom13@gmail.com>
2021-12-29 21:22:44 +0900

    feat(file): add async find

--- src/file/find-file.ts
+++ src/file/find-file.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 import * as path from 'path';
 
 /**
@@ -27,6 +27,38 @@ export const find = (startPath: string, ignore: string | RegExp = ''): string[] 
       .forEach(f => {
         recur(path.join(curPath, f.name));
       });
+  }
+  return result;
+};
+
+/**
+ * same as unix command 'find'
+ */
+export const findAsync = async (startPath: string, ignore: string | RegExp = ''): Promise<string[]> => {
+  const regex = typeof ignore === 'string' ? new RegExp(ignore) : ignore;
+  const result: string[] = [];
+  await recur(startPath);
+
+  async function recur(curPath: string): Promise<void> {
+    const children = await fs.readdir(curPath, { withFileTypes: true });
+    await Promise.all(
+      children
+        .filter(f => {
+          if (ignore && regex.test(f.name)) {
+            return false;
+          } else if (/\.git/.test(f.name) || /node_modules/.test(f.name)) {
+            return false;
+          } else if (f.isFile()) {
+            result.push(path.join(curPath, f.name));
+            return false;
+          } else {
+            return true;
+          }
+        })
+        .map(async f => {
+          await recur(path.join(curPath, f.name));
+        })
+    );
   }
   return result;
 };
